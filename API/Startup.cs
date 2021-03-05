177cf0b250e67cfe1bd3c963dd5fe9fb12a563ff
pavel <pavelkolom@gmail.com>
2021-03-05 12:41:44 +0400

    Add SPA project

--- API/Startup.cs
+++ API/Startup.cs
@@ -50,6 +50,8 @@ namespace API
       {
         c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
       });
+
+      services.AddCors();
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -65,6 +67,8 @@ namespace API
       app.UseHttpsRedirection();
 
       app.UseRouting();
+      
+      app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200", "https://localhost:44376"));
 
       app.UseAuthorization();
 
